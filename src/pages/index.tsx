a08ba482f95b04c1b15e9414679e025470bcfb85
Kallan Jones <kallan@alphero.com>
2020-08-24 17:12:51 +1200

    Rename index.js to index.tsx Create Home.scss file.

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Header } from 'components/Header/Header';
+
+import './Home.scss';
+
+export default function Home() {
+  return (
+    <main className="Home">
+      <Header />
+
+      <h1>Home</h1>
+    </main>
+  );
+}
