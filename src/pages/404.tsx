4a216724db7f5e3fa37caf6f01136adc1c75c918
Kallan Jones <kallan@alphero.com>
2020-08-24 17:13:09 +1200

    Create 404 and checkout page.

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Header } from "components/Header/Header";
+
+export default function FourOhFour() {
+  return (
+    <main className="FourOhFour">
+      <Header />
+
+      <h1>404</h1>
+    </main>
+  );
+}
---
+++ src/pages/checkout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Header } from "../components/Header/Header";
+
+export default function Checkout() {
+  return (
+    <main className="Checkout">
+      <Header />
+
+      <h1>Checkout</h1>
+    </main>
+  );
+}
