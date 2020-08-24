f577edd776de4d7dcfe647089ac0a4aa7d38b32b
Kallan Jones <kallan@alphero.com>
2020-08-24 17:12:10 +1200

    Update gatsby-config to include a solid base of gatsby plugins.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,43 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+const path = require("path");
+const appVersion = require("./package.json").version;
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  siteMetadata: {
+    // title,
+    // description: "",
+    // author: "",
+    appVersion,
+  },
+  plugins: [
+    "gatsby-alias-imports",
+    {
+      resolve: "gatsby-plugin-react-helmet",
+    },
+    {
+      resolve: "gatsby-plugin-sass",
+      options: {
+        implementation: require("sass"),
+        useResolveUrlLoader: true,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-root-import",
+      options: {
+        "@app": path.join(__dirname, "src/"),
+        public: path.join(__dirname, "public/"),
+        pages: path.join(__dirname, "src/pages"),
+        components: path.join(__dirname, "src/components"),
+      },
+    },
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-XXXXXXXXX-X",
+      },
+    },
+  ],
+};
