b5af2177aeffe24347595452396d01717f6636d0
Kallan Jones <kallan@alphero.com>
2020-08-25 21:22:37 +1200

    Create gatsby-browser.js file to wrap all pages with Layout component.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,11 @@
+import React from 'react';
+
 // Import app styles into every page
-import "./src/styles/app.scss";
+import './src/styles/app.scss';
+
+import { Layout } from './src/Layout/Layout';
+
+// Wraps every page in a component
+export const wrapPageElement = ({ element, props }) => {
+  return <Layout {...props}>{element}</Layout>;
+};
