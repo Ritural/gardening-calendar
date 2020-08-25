5582393d5389ac3935ed8117350ea782aea10b74
Kallan Jones <kallan@alphero.com>
2020-08-25 22:02:21 +1200

    Update Footer component to take the version number as a prop.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { version } from 'react';
 
 import './Footer.scss';
 
-interface Props {}
+interface Props {
+  version: string;
+}
 
-export const Footer = (props: Props) => {
-  const {} = props;
-
+export const Footer = ({ version }: Props) => {
   return (
     <footer className="Footer">
       <p>Made with Tony by Kallan</p>
+      <p>Version {version}</p>
     </footer>
   );
 };
